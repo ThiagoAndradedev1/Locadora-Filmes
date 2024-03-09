9f31fa8caeabcaa21f4700ec26f93c1f021e32d3
Thiago <thiago.andrade.dev1@gmail.com>
2024-03-09 19:14:39 -0300

    Estiliza RegisterclientView com novo padrão

--- src/views/RegisterclientView.vue
+++ src/views/RegisterclientView.vue
@@ -1,221 +1,348 @@
 <template>
-  <div class="flex justify-center items-center h-full">
-    <div class="w-full max-w-md">
-      <h1 class="text-2xl mb-2 mt-6">Criar Cliente</h1>
-      <form @submit.prevent="createCliente">
-        <div class="mb-4">
-          <label for="nome" class="block text-gray-700 font-bold mb-2">Nome:</label>
-          <input
-            v-model.trim="cliente.nome"
-            type="text"
-            id="nome"
-            name="nome"
-            class="form-input"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label for="sobrenome" class="block text-gray-700 font-bold mb-2">Sobrenome:</label>
-          <input
-            v-model.trim="cliente.sobrenome"
-            type="text"
-            id="sobrenome"
-            name="sobrenome"
-            class="form-input"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label for="cpf" class="block text-gray-700 font-bold mb-2">CPF:</label>
-          <input
-            v-model.trim="cliente.cpf"
-            type="text"
-            id="cpf"
-            name="cpf"
-            class="form-input"
-            required
-            maxlength="14"
-            @input="formatCPF"
-          />
-        </div>
-        <div class="mb-4">
-          <label for="email" class="block text-gray-700 font-bold mb-2">E-mail:</label>
-          <input
-            v-model.trim="cliente.contatos.email"
-            type="email"
-            id="email"
-            name="email"
-            class="form-input"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label for="celular" class="block text-gray-700 font-bold mb-2">Celular:</label>
-          <input
-            v-model.trim="cliente.contatos.celular"
-            type="tel"
-            id="celular"
-            name="celular"
-            class="form-input"
-            @input="formatCellPhone"
-            maxlength="15"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label for="cep" class="block text-gray-700 font-bold mb-2">CEP:</label>
-          <input
-            v-model.trim="cliente.cep"
-            type="text"
-            id="cep"
-            name="cep"
-            class="form-input"
-            required
-            @input="findAddress"
-            maxlength="9"
-          />
-        </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 font-bold mb-2">Logradouro:</label>
-          <input
-            v-model.trim="cliente.endereco.logradouro"
-            type="text"
-            class="form-input"
-            :disabled="!fullAddress"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 font-bold mb-2">Bairro:</label>
-          <input
-            v-model.trim="cliente.endereco.bairro"
-            type="text"
-            class="form-input"
-            :disabled="!fullAddress"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 font-bold mb-2">Cidade:</label>
-          <input
-            v-model.trim="cliente.endereco.cidade"
-            type="text"
-            class="form-input"
-            :disabled="!fullAddress"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 font-bold mb-2">UF:</label>
-          <input
-            v-model.trim="cliente.endereco.uf"
-            type="text"
-            class="form-input"
-            :disabled="!fullAddress"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Criar Cliente
-        </button>
-      </form>
+  <div class="min-h-screen flex flex-col items-center justify-center font-mono mt-10 mb-10">
+    <h1 class="text-3xl font-bold mb-4">Registrar Cliente</h1>
+    <div
+      class="flex flex-col bg-white shadow-lg px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md"
+    >
+      <div class="mt-10">
+        <form @submit="onSubmit" novalidate>
+          <div class="flex flex-col mb-6">
+            <label for="name" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Nome</label
+            >
+
+            <input
+              v-model="name"
+              v-bind="nameAttrs"
+              id="name"
+              type="text"
+              name="name"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o nome"
+            />
+            <div class="text-red-400 text-sm">{{ errors.name }}</div>
+          </div>
+          <div class="flex flex-col mb-6">
+            <label for="document" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Sobrenome</label
+            >
+
+            <input
+              v-model="surname"
+              v-bind="surnameAttrs"
+              id="surname"
+              type="text"
+              name="surname"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o Sobrenome"
+            />
+            <div class="text-red-400 text-sm">{{ errors.surname }}</div>
+          </div>
+
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >CPF</label
+            >
+
+            <input
+              v-model="cpf"
+              v-bind="cpfAttrs"
+              id="cpf"
+              type="text"
+              maxlength="14"
+              name="cpf"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o CPF"
+            />
+            <div class="text-red-400 text-sm">{{ errors.cpf }}</div>
+          </div>
+
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Email</label
+            >
+
+            <input
+              v-model="email"
+              v-bind="emailAttrs"
+              id="email"
+              type="text"
+              name="email"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o Email"
+            />
+            <div class="text-red-400 text-sm">{{ errors.email }}</div>
+          </div>
+
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Celular</label
+            >
+
+            <input
+              v-model="cellphone"
+              v-bind="cellphoneAttrs"
+              maxlength="15"
+              id="cellphone"
+              type="text"
+              name="cellphone"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o Celular"
+            />
+            <div class="text-red-400 text-sm">{{ errors.cellphone }}</div>
+          </div>
+
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >CEP</label
+            >
+
+            <input
+              v-model="cep"
+              v-bind="cepAttrs"
+              maxlength="9"
+              @input="findAddress"
+              id="cep"
+              type="text"
+              name="cep"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o CEP"
+            />
+            <div class="text-red-400 text-sm">{{ errors.cep }}</div>
+          </div>
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Logradouro</label
+            >
+
+            <input
+              v-model="logradouro"
+              v-bind="logradouroAttrs"
+              :disabled="true"
+              id="logradouro"
+              type="text"
+              name="logradouro"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o Logradouro"
+            />
+            <div class="text-red-400 text-sm">{{ errors.logradouro }}</div>
+          </div>
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Bairro</label
+            >
+
+            <input
+              v-model="bairro"
+              v-bind="bairroAttrs"
+              :disabled="true"
+              id="bairro"
+              type="text"
+              name="bairro"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o Bairro"
+            />
+            <div class="text-red-400 text-sm">{{ errors.bairro }}</div>
+          </div>
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >Cidade</label
+            >
+
+            <input
+              v-model="cidade"
+              inva
+              v-bind="cidadeAttrs"
+              :disabled="true"
+              id="cidade"
+              type="text"
+              name="cidade"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe a Cidade"
+            />
+            <div class="text-red-400 text-sm">{{ errors.cidade }}</div>
+          </div>
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >UF</label
+            >
+
+            <input
+              v-model="uf"
+              v-bind="ufAttrs"
+              :disabled="true"
+              id="uf"
+              type="text"
+              name="uf"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o UF"
+            />
+            <div class="text-red-400 text-sm">{{ errors.uf }}</div>
+          </div>
+          <div class="flex w-full">
+            <button
+              class="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in"
+            >
+              <span class="mr-2 uppercase">Registrar!</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useForm } from 'vee-validate'
+import { object, string } from 'yup'
+import { toTypedSchema } from '@vee-validate/yup'
+import { watch } from 'vue'
 import type { Client } from '@/data/models/Client.model'
-import { ref, watch } from 'vue'
 
-const cliente = ref<Client>({
-  id: 0,
-  nome: '',
-  sobrenome: '',
-  cpf: '',
-  contatos: {
-    email: '',
-    celular: ''
-  },
-  endereco: {
-    logradouro: '',
-    bairro: '',
-    cidade: '',
-    uf: ''
-  },
-  cep: '',
-  status: false,
-  isAlocated: false
+const { handleSubmit, defineField, errors, setValues } = useForm({
+  validationSchema: toTypedSchema(
+    object({
+      name: string()
+        .min(4, 'O nome deve ter pelo menos 4 caracteres')
+        .required('O campo nome é obrigatório'),
+      surname: string()
+        .min(4, 'A senha deve ter pelo menos 4 caracteres')
+        .required('O campo sobrenome é obrigatório'),
+      cpf: string()
+        .matches(
+          /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+          'O CPF deve estar no formato correto (XXX.XXX.XXX-XX)'
+        )
+        .required('O campo CPF é obrigatório'),
+      email: string()
+        .email('O email deve estar em um formato válido')
+        .required('O campo email é obrigatório'),
+      cellphone: string()
+        .matches(
+          /^\(\d{2}\) \d{5}-\d{4}$/,
+          'O número de celular deve estar no formato correto (XX) XXXXX-XXXX'
+        )
+        .required('O campo número de celular é obrigatório'),
+      cep: string()
+        .matches(/^\d{5}-\d{3}$/, 'O CEP deve estar no formato correto (XXXXX-XXX)')
+        .required('O campo CEP é obrigatório'),
+      logradouro: string()
+        .min(4, 'O logradouro deve ter pelo menos 4 caracteres')
+        .required('O campo logradouro é obrigatório'),
+      bairro: string()
+        .min(4, 'O bairro deve ter pelo menos 4 caracteres')
+        .required('O campo bairro é obrigatório'),
+      cidade: string()
+        .min(4, 'A cidade deve ter pelo menos 4 caracteres')
+        .required('O campo cidade é obrigatório'),
+      uf: string()
+        .min(1, 'O UF deve ter pelo menos 1 caracter')
+        .required('O campo uf é obrigatório')
+    })
+  )
 })
 
-const clientes = ref<Client[]>(JSON.parse(localStorage.getItem('clientes') || '[]'))
-
-const fullAddress = ref(false)
-
-const formValid = ref(false)
-
-const createCliente = () => {
-  clientes.value.push({ ...cliente.value, id: clientes.value.length + 1, cep: cliente.value.cep })
-
-  localStorage.setItem('clientes', JSON.stringify(clientes.value))
-}
+const [name, nameAttrs] = defineField('name')
+const [surname, surnameAttrs] = defineField('surname')
+const [cpf, cpfAttrs] = defineField('cpf')
+const [email, emailAttrs] = defineField('email')
+const [cellphone, cellphoneAttrs] = defineField('cellphone')
+const [cep, cepAttrs] = defineField('cep')
+const [logradouro, logradouroAttrs] = defineField('logradouro')
+const [bairro, bairroAttrs] = defineField('bairro')
+const [cidade, cidadeAttrs] = defineField('cidade')
+const [uf, ufAttrs] = defineField('uf')
 
 const findAddress = async () => {
-  if (cliente.value.cep.length === 8) {
+  if (cep.value?.length === 9) {
     try {
-      const response = await fetch(`https://viacep.com.br/ws/${cliente.value.cep}/json`)
+      const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json`)
       const data = await response.json()
       if (!data.erro) {
-        cliente.value.endereco.logradouro = data.logradouro
-        cliente.value.endereco.bairro = data.bairro
-        cliente.value.endereco.cidade = data.localidade
-        cliente.value.endereco.uf = data.uf
-        fullAddress.value = true
-      } else {
-        fullAddress.value = false
+        setValues({
+          logradouro: data.logradouro,
+          bairro: data.bairro,
+          cidade: data.localidade,
+          uf: data.uf
+        })
       }
     } catch (error) {
       console.error('Erro ao buscar endereço:', error)
-      fullAddress.value = false
     }
-  } else {
-    fullAddress.value = false
   }
 }
 
+const onSubmit = handleSubmit(
+  async ({ name, surname, cpf, email, cellphone, cep, logradouro, bairro, cidade, uf }) => {
+    const newClient: Client = {
+      id: Math.floor(Math.random() * 10000),
+      nome: name,
+      sobrenome: surname,
+      cpf,
+      contatos: {
+        email,
+        celular: cellphone
+      },
+      endereco: {
+        logradouro,
+        bairro,
+        cidade,
+        uf
+      },
+      cep,
+      status: true,
+      isAlocated: false
+    }
+
+    const clientsStorage = localStorage.getItem('clients')
+    let currentClients: Client[] = clientsStorage ? (JSON.parse(clientsStorage) as Client[]) : []
+
+    const clientAlreadyExists = currentClients.find((client) => client.cpf === newClient.cpf)
+
+    if (clientAlreadyExists) {
+      alert('Este cliente já está cadastrado!')
+      return
+    }
+
+    currentClients = [...currentClients, newClient]
+
+    localStorage.setItem('clients', JSON.stringify(currentClients))
+
+    alert('Cliente registrado com sucesso!')
+  }
+)
+
 watch(
-  () => cliente.value.cep,
+  () => cep,
   () => findAddress()
 )
 
-const formatCPF = () => {
-  let cpf = cliente.value.cpf.replace(/\D/g, '')
-  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2')
-  cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2')
-  cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2')
-
-  cliente.value.cpf = cpf
-}
-
-const formatCellPhone = () => {
-  let celular = cliente.value.contatos.celular.replace(/\D/g, '')
-
-  celular = celular.replace(/^(\d{2})(\d)/g, '($1) $2')
-  celular = celular.replace(/(\d{5})(\d)/, '$1-$2')
-
-  cliente.value.contatos.celular = celular
-}
-
-watch(
-  () => cliente.value,
-  () => {
-    formValid.value = Object.values(cliente.value).every((value) => value !== '')
+watch(cpf, () => {
+  if (cpf.value) {
+    const cleanedCPF = cpf.value.replace(/\D/g, '')
+    cpf.value = cleanedCPF.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
   }
-)
+})
+
+watch(email, () => {
+  if (email.value) {
+    email.value = email.value.trim()
+  }
+})
+
+watch(cellphone, () => {
+  if (cellphone.value) {
+    const cleanedCellphone = cellphone.value.replace(/\D/g, '')
+    cellphone.value = cleanedCellphone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3')
+  }
+})
+
+watch(cep, () => {
+  if (cep.value) {
+    const cleanedCEP = cep.value.replace(/\D/g, '')
+    cep.value = cleanedCEP.replace(/(\d{5})(\d{3})/, '$1-$2')
+  }
+})
 </script>
 
-<style scoped>
-/* Estilos personalizados podem ser adicionados aqui */
-</style>
+<style scoped></style>
