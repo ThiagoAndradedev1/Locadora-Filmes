7be154bf9f1449999e8d47237fd09fe36d538c53
Thiago <thiago.andrade.dev1@gmail.com>
2024-03-09 15:20:58 -0300

    Estiliza LoginView com novo padrão

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,82 +1,87 @@
 <template>
-  <div>
-    <div class="max-w-2xl m-auto bg-gray-50 p-20">
-      <form @submit="onSubmit">
-        <div>
-          <div>
-            <label
-              for="document"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 mt-4"
-              >Documento</label
+  <div class="min-h-screen flex flex-col items-center justify-center font-mono">
+    <h1 class="text-3xl font-bold mb-4">Login</h1>
+    <div
+      class="flex flex-col bg-white shadow-lg px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md"
+    >
+      <div class="mt-10">
+        <form @submit="onSubmit" novalidate>
+          <div class="flex flex-col mb-6">
+            <label for="document" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
+              >CPF</label
             >
+
             <input
               v-model="document"
               v-bind="documentAttrs"
-              type="text"
               id="document"
-              class="w-full h-12 px-4 mb-2 text-lg text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              placeholder="Informe seu documento"
-              required
+              type="text"
+              name="document"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe o CPF"
             />
-            <div class="text-red-600 text-sm">{{ errors.document }}</div>
+            <div class="text-red-400 text-sm">{{ errors.document }}</div>
           </div>
-        </div>
 
-        <div>
-          <div>
-            <label
-              for="password"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 mt-4"
+          <div class="flex flex-col mb-6">
+            <label for="password" class="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
               >Senha</label
             >
+
             <input
               v-model="password"
               v-bind="passwordAttrs"
-              type="password"
               id="password"
-              class="w-full h-12 px-4 mb-2 text-lg text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              placeholder="Informe sua senha"
-              required
+              type="password"
+              name="password"
+              class="text-sm sm:text-base placeholder-gray-500 pl-4 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+              placeholder="Informe a senha"
             />
-            <div class="text-red-600 text-sm">{{ errors.password }}</div>
+            <div class="text-red-400 text-sm">{{ errors.password }}</div>
           </div>
-        </div>
-
-        <button
-          class="w-full mt-4 h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
-        >
-          Login
-        </button>
-      </form>
+          <div class="flex w-full">
+            <button
+              class="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in"
+            >
+              <span class="mr-2 uppercase">Logar!</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { User } from '@/data/models/User.model'
 import { useForm } from 'vee-validate'
-import * as yup from 'yup'
+import { object, string } from 'yup'
+import { toTypedSchema } from '@vee-validate/yup'
+import type { User } from '@/data/models/User.model'
 
-const { errors, handleSubmit, defineField } = useForm({
-  validationSchema: yup.object({
-    document: yup.string().min(1).required(),
-    password: yup.string().min(1).required()
-  })
+const { handleSubmit, defineField, errors } = useForm({
+  validationSchema: toTypedSchema(
+    object({
+      password: string()
+        .min(4, 'A senha deve ter pelo menos 4 caracteres')
+        .required('O campo password é obrigatório'),
+      document: string()
+        .min(11, 'O CPF deve ter 11 digitos')
+        .max(11, 'O CPF deve ter 11 digitos')
+        .required('O campo CPF é obrigatório')
+    })
+  )
 })
 
-const [document, documentAttrs] = defineField('document')
 const [password, passwordAttrs] = defineField('password')
+const [document, documentAttrs] = defineField('document')
 
-const onSubmit = handleSubmit((values): void => {
-  const documentInputValue = String(values.document)
-  const passwordInputValue = String(values.password)
-
+const onSubmit = handleSubmit(({ password, document }) => {
   const usersStorage = localStorage.getItem('users')
 
   let actualUsers: User[] = usersStorage ? (JSON.parse(usersStorage) as User[]) : []
 
   const user = actualUsers.find(
-    (usuario) => usuario.document === documentInputValue && usuario.password === passwordInputValue
+    (usuario) => usuario.document === document && usuario.password === password
   )
 
   if (user?.status === false) {
